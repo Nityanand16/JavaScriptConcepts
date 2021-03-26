b00853de277a360a6e0b91b7d12c581fb074274e
Nityanand Gupta <nityanandgupta@Nityanands-MacBook-Pro.local>
2021-03-26 18:38:24 +0100

    first commit

---
+++ variables.js
@@ -0,0 +1,47 @@
+var age = 34;
+alert(age);
+
+let age = 34; //In new Javascript we can use let in place of var
+alert(age);
+
+var nationality = "Indian";
+alert(nationality);
+
+var status = true;
+alert(status);
+
+var a;
+a=78;
+console.log(a);
+
+//---------------------------------------------------------------------------
+
+var age = 34;
+console.log(age);
+
+age = 36;   //we update or change the value of variables
+console.log(age);
+
+//--------------------------------------------------------------------------
+
+const age = 34;
+console.log(age);
+
+age = 36;   //we cany update or change the value of variables as now the variable is constant
+console.log(age);
+
+//--------------------------------------------------------------------------
+
+//Type of Operator, We can check The Data Type
+var age = 34;
+console.log(typeof age);    //Number
+
+var name = "Hi";
+console.log(typeof name);   //String
+
+var status = true;
+console.log(typeof status); //Boolean
+
+status = 34;
+console.log(typeof status); //Number
+
