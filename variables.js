d47528635506e50b4ab91518204fc6d1443bc710
Nityanand Gupta <nityanandgupta@Nityanands-MacBook-Pro.local>
2021-03-28 20:09:18 +0200

    update file

--- variables.js
+++ variables.js
@@ -1,47 +1,58 @@
-var age = 34;
-alert(age);
+// var age = 34;
+// alert(age);
 
-let age = 34; //In new Javascript we can use let in place of var
-alert(age);
+// let age = 34; //In new Javascript we can use let in place of var
+// alert(age);
 
-var nationality = "Indian";
-alert(nationality);
+// var nationality = "Indian";
+// alert(nationality);
 
-var status = true;
-alert(status);
+// var status = true;
+// alert(status);
 
-var a;
-a=78;
-console.log(a);
+// var a;
+// a=78;
+// console.log(a);
 
-//---------------------------------------------------------------------------
+// //---------------------------------------------------------------------------
 
-var age = 34;
-console.log(age);
+// var age = 34;
+// console.log(age);
 
-age = 36;   //we update or change the value of variables
-console.log(age);
+// age = 36;   //we update or change the value of variables
+// console.log(age);
 
-//--------------------------------------------------------------------------
+// //---------------------------------------------------------------------------
+// //Difference between var and let
 
-const age = 34;
-console.log(age);
+// console.log(x); //undefined
+// var x = 100;
+// console.log(x);
 
-age = 36;   //we cany update or change the value of variables as now the variable is constant
-console.log(age);
+console.log(x); //error
+let x = 100;
+console.log(x);
 
-//--------------------------------------------------------------------------
+// //--------------------------------------------------------------------------
 
-//Type of Operator, We can check The Data Type
-var age = 34;
-console.log(typeof age);    //Number
+// const age = 34;
+// console.log(age);
 
-var name = "Hi";
-console.log(typeof name);   //String
+// age = 36;   //we can not update or change the value of variables as now the variable is constant
+// console.log(age);
 
-var status = true;
-console.log(typeof status); //Boolean
+// //--------------------------------------------------------------------------
 
-status = 34;
-console.log(typeof status); //Number
+// //Type of Operator, We can check The Data Type
+// var age = 34;
+// console.log(typeof age);    //Number
+
+// var name = "Hi";
+// console.log(typeof name);   //String
+
+// var status = true;
+// console.log(typeof status); //Boolean
+
+// status = 34;
+// console.log(typeof status); //Number
 
