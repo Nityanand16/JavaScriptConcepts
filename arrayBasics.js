2301f0a01fd0bc0bcbd899e48c8e60356b0026e0
Nityanand16 <nityanandgupta@yahoo.com>
2021-04-21 16:20:24 +0200

    test file

---
+++ arrayBasics.js
@@ -0,0 +1,16 @@
+let cars=["Audi", "BMW", "Volkswagon", "Volvo", "Nissan"];
+//or
+//let cars = new Array("Audi", "BMW", "Volkswagon", "Volvo", "Nissan");
+
+console.log(cars);
+
+//access the value base on index
+console.log(cars[1]);
+
+//change the index value
+cars[3]="Mercedes Benz"
+console.log(cars);
+
+//we can have different types of variables in the same array
+let student=["Nitya", 34, 5.5, true]    //string, integer, float, boolean
+console.log(student);
