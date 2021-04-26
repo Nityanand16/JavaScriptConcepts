178d1fcf648e8e9e46838b541ac76803aee9696c
Nityanand16 <nityanandgupta@yahoo.com>
2021-04-26 22:00:05 +0200

    update file

--- arrayBasics.js
+++ arrayBasics.js
@@ -15,3 +15,36 @@ console.log(cars);
 let student=["Nitya", 34, 5.5, true]    //string, integer, float, boolean
 console.log(student);
 
+//JavaScript array also holds object
+let car1={
+    carName: "Audi",
+    price: 400000
+};
+let car2={
+    carName: "BMW",
+    price: 500000
+};
+let vehicle=[car1, car2];
+
+console.log(vehicle);
+console.log(vehicle[1]);
+
+console.log("*************************")
+
+let city = ["Copenhagen", "Arhus", "Gladsaxe", "Rodovre", "Soborg"]
+console.log(city.length)
+
+// //Looping elements for array (for loop)
+// for (let i = 0; i <= city.length-1; i++) {
+//     console.log(city[i]);
+// };
+
+//Looping elements for array (for/of loop) this is specially designed for array
+for(ele of city){
+    console.log(ele);
+};
+
+//Recognize an Array-typeof
+console.log(typeof(city));  //object
+
+console.log(Array.isArray(city));   //true
